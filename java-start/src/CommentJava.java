9a1349eb0555821a09165b795ae5d4d2df2abe51
Yeoni <siunaus224@gmail.com>
2023-12-26 20:36:21 +0900

    231226

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,10 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력합니다요
+        /*
+        System.out.println("hello java1");
+        System.out.println("hello java1");
+        여러줄 주석처리
+        */
+    }
+}
---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava { // 클래스 : 파일명과 클래스 이름이 같아야 한다
+    // 중괄호 블록을 사용해서 클래스의 시작과 끝을 나타낸다
+    public static void main(String[] args) { // 메서드 : 자바는 main(String[] args) 메서드를 찾아서 프로그램을 시작한다
+        System.out.println("hello java"); // 문자열 사용시 쌍따옴표 사용, 문장이 끝나면 세미콜론을 필수로
+    }
+}
