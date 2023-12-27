67e8f80dfdf6460197443fac4301450a908f28ba
Yeoni <siunaus224@gmail.com>
2023-12-28 01:00:54 +0900

    231228

---
+++ java-start/src/operator/Operator1.java
@@ -0,0 +1,29 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("a + b = " + sum); // a + b = 7
+
+        // 뺄셈
+        int diff = a - b;
+        System.out.println("a - b = " + diff); // a - b = 3
+
+        // 곱셈
+        int multi = a * b;
+        System.out.println("a * b = " + multi); // a * b = 10
+
+        // 나눗셈
+        int div = a / b;
+        System.out.println("a / b = " + div); // a / b = 2 : int끼리의 계산은 소수점 표현이 안됨
+
+        // 나머지
+        int mod = a % b;
+        System.out.println("a % b = " + mod); // a % b = 1
+    }
+}
