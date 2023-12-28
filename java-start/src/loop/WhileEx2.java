9a0197a7ebf5a90ff9087d6df78179336dce19b8
Yeoni <siunaus224@gmail.com>
2023-12-28 18:53:46 +0900

    반복문

---
+++ java-start/src/loop/WhileEx2.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int i = 1;
+        int count = 0;
+
+        while (count < 10) {
+            if (i%2 == 0) {
+                System.out.println(i);
+                count++;
+            }
+            i++;
+        }
+    }
+}
