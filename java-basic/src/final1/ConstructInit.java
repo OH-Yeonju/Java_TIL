8fa44e94ad67f54121ad565d98f1dc9a1729b02d
Yeoni <siunaus224@gmail.com>
2024-01-03 03:23:18 +0900

    final

---
+++ java-basic/src/final1/ConstructInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;
+
+    // 초기값 없을 경우 생성자로 초기화 가능
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+}
---
+++ java-basic/src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+    // 초기값 이미 들어가 있을 경우 생성자로 초기화 안됨
+//    public FieldInit(int value) {
+//        this.value = value;
+//    }
+}
